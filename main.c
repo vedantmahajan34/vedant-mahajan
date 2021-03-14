c496eae03d1df1e141aba712b47b816cb36f24bb
vedantmahajan34 <80624852+vedantmahajan34@users.noreply.github.com>
2021-03-14 20:03:09 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -3,28 +3,21 @@
 
 int main()
 {
-    printf("1.pizza\n2.burger\n3.pasta\n4.french fries\n5.sandwich");
-    printf("\ncode");
-    int code;
-    scanf("%d",&code);
-
-    switch(code)
- {case 1 :
-        printf("food item-pizza\nprice-Rs239");
-        break;
-    case 2:
-        printf("food item-burger\nprice-Rs 129");
-            break;
-    case 3:
-        printf("food item-pasta\nprice-Rs179");
-        break;
-    case 4:
-        printf("food item-french fries\nprice-Rs99");
-       break;
-    case 5:
-        printf("food item-sandwich\nprice-Rs 149");
-        break;
-    default:
-        printf("invalid choice");}
-    return 0;
+    int m;
+    printf("enter the percentage",m);
+    scanf("%d",&m);
+    if (m>85)
+            printf("grade-A");
+    if (m>70 && m<84)
+            printf("grade-B");
+    if (m>55 && m<69)
+            printf("grade-C");
+    if (m>40 && m<54)
+            printf("grade-D");
+    if (m<40)
+        printf("grade-f");
 }
+
+
+
+
